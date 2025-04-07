59257620eadf06587c421815173a48d7a191be42
Plazma <vdzlucas@gmail.com>
2025-04-07 21:03:03 +0200

    Handling hosting request and creation of migration for hosting game

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,4 +13,7 @@ class User extends Model
     protected $fillable = [
         'name', 'email', 'password',
     ];
+    public function game(){
+        return $this->belongsTo(HostedGame::class);
+    }
 }
